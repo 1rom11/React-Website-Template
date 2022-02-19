f9ab09882623aecc259ccc283766528337e556b1
Dio-1rom11 <76579908+1rom11@users.noreply.github.com>
2022-02-19 06:35:16 +0000

    Build started

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
  * @type { import('vite').UserConfig }
  */
 export default {
+  base: '/React-Website-Template/',
   plugins: [reactRefresh()],
   server: {
     host: '0.0.0.0',
